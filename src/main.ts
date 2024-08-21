7d4e41f83df9a9c2d8dee4b335ba00792821d997
StivenBant <stiven_ramos@bantrab.com>
2024-08-21 11:09:06 -0600

    Update main.ts despliegue port

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  const port = process.env.PORT || 3000;
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
